af325295cca1d2d0578d1316bf8fc65bcf28a86e
Explorer <propokerstar90@gmail.com>
2018-09-15 14:48:47 +0300

    Added mutation

--- server.js
+++ server.js
@@ -10,10 +10,31 @@ const typeDefs = gql`
         task: String
         completed: Boolean
     }
+    
+    type Query{
+        getTodos:[Todo]
+    }
+    
+    type Mutation{
+        addTodo(task: String, completed: Boolean): Todo
+    }
 `;
 
+const resolvers = {
+    Query: {
+        getTodos: () => todos
+    },
+    Mutation:{
+        addTodo: (_, { task, completed }) => {
+            const todo = {task, completed};
+            todos.push(todo);
+            return todo;
+        }
+    }
+};
+
 const server = new ApolloServer({
-    typeDefs: typeDefs
+    typeDefs, resolvers
 });
 
 server.listen().then(({ url }) => {
