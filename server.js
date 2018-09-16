361b6f6a96f20029e5e2c87d401edf2528a5914b
Explorer <propokerstar90@gmail.com>
2018-09-16 11:12:57 +0300

    Created mogoose schemas

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
+const User = require('./models/User');
+const Post = require('./models/Post');
 
 mongoose
     .connect(process.env.MONGO_URI, { userNewUrlParser: true })
@@ -19,7 +21,11 @@ const typeDefs = gql`
 `;
 
 const server = new ApolloServer({
-    typeDefs
+    typeDefs,
+    context: {
+        User,
+        Post
+    }
 });
 
 server.listen().then(({ url }) => {
