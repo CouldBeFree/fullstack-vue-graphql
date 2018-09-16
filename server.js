a438294d18373b1315c6e8f142b19c0286f14643
Explorer <propokerstar90@gmail.com>
2018-09-16 18:39:33 +0300

    Added client

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 
+// Import typeDefs and resolvers
 const filePath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
 const resolvers = require("./resolvers");
 
+// Import Environment variables and Mongoose Models
 require('dotenv').config({ path: 'variables.env' });
 const User = require('./models/User');
 const Post = require('./models/Post');
 
+// Connect to MLab Database
 mongoose
     .connect(process.env.MONGO_URI, { userNewUrlParser: true })
     .then(() => console.log('DB connected'))
     .catch(err => console.log(err));
 
 
-
+// Create Apollo GraphQL server using typeDefs, resolvers
 const server = new ApolloServer({
     typeDefs,
     resolvers,
