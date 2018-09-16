0d691658ba73e70d005d9e80221d2b8f664b7002
Explorer <propokerstar90@gmail.com>
2018-09-16 15:06:41 +0300

    Enabled signUp mutation

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const resolvers = require("./resolvers");
+
 require('dotenv').config({ path: 'variables.env' });
 const User = require('./models/User');
 const Post = require('./models/Post');
@@ -9,19 +16,11 @@ mongoose
     .then(() => console.log('DB connected'))
     .catch(err => console.log(err));
 
-const typeDefs = gql`
-    type Todo{
-        task: String
-        completed: Boolean
-    }
-    
-    type Query{
-        getTodos:[Todo]
-    }
-`;
+
 
 const server = new ApolloServer({
     typeDefs,
+    resolvers,
     context: {
         User,
         Post
