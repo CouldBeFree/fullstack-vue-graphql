d7c3ad929a827ff159e329947b871d110a31e2c9
Explorer <propokerstar90@gmail.com>
2018-09-22 21:52:16 +0300

    Added apollo boost, enabled query in the Home component

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const { ApolloServer } = require("apollo-server");
-const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 
 // Import typeDefs and resolvers
-const filePath = path.join(__dirname, 'typeDefs.gql');
-const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
 const resolvers = require("./resolvers");
 
-// Import Environment variables and Mongoose Models
-require('dotenv').config({ path: 'variables.env' });
-const User = require('./models/User');
-const Post = require('./models/Post');
+// Import Environment Variables and Mongoose Models
+require("dotenv").config({ path: "variables.env" });
+const User = require("./models/User");
+const Post = require("./models/Post");
 
 // Connect to MLab Database
 mongoose
-    .connect(process.env.MONGO_URI, { userNewUrlParser: true })
-    .then(() => console.log('DB connected'))
-    .catch(err => console.log(err));
+    .connect(
+        process.env.MONGO_URI,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("DB connected"))
+    .catch(err => console.error(err));
 
-
-// Create Apollo GraphQL server using typeDefs, resolvers
+// Create Apollo/GraphQL Server using typeDefs, resolvers, and context object
 const server = new ApolloServer({
     typeDefs,
     resolvers,
