42747cfcdcd421aa40815112db1cd3c5dee4dd29
Explorer <propokerstar90@gmail.com>
2018-09-23 15:42:07 +0300

    Enabled token

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,10 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const createToken = (user, secret, expiresIn) => {
+  const { username, email } = user;
+  return jwt.sign({ username, email }, secret, { expiresIn });
+};
 
 module.exports = {
     Query: {
@@ -32,7 +38,7 @@ module.exports = {
             if(!isValidPassword){
                 throw new Error('Invalid password');
             }
-            return user;
+            return { token: createToken(user, process.env.SECRET, '1hr')};
         },
         signupUser: async(_, {username, email, password}, {User}) => {
             const user = await User.findOne({ username });
@@ -44,7 +50,7 @@ module.exports = {
                 email,
                 password
             }).save();
-            return newUser
+            return { token: createToken(newUser, process.env.SECRET, '1hr')};
         }
     }
 };
